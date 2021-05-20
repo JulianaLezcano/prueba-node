a90cf372ba63e23253cd17b93e1c3b0d7f520fab
Julianalezcano <juliana.v.lezcano@hotmail.com>
2021-05-20 10:32:02 -0300

    Mensaje

---
+++ app.js
@@ -0,0 +1,8 @@
+/*para iniciar proyecto git init
+para agregar archivo git add nombre de archivo
+para agregar mas de una archivo git add .
+git commit: rotula lo que vamos haciendo, tiene que ser descrptivo, preciso y corto...
+primero se pasan credenciales: 
+git config --global user.name 'Julianalezcano'
+git config --global user.email 'juliana.v.lezcano@hotmail.com'
+git commit -m y el msj, si es mas de una palabra va con 'Agrega archivos base'
