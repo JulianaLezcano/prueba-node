dee1f3029f2863277ecd29a56e47ac7baed60f21
Julianalezcano <juliana.v.lezcano@hotmail.com>
2021-05-24 07:44:15 -0300

    Modifica Juliana

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ todo en consola:
 para iniciar proyecto  git init .... 
 master: rama principal del proyecto, carpeta inicializadora
 el archivo app.js aparece con una U al lado que significa que no esta guardado/agregado al master
--git status: aparece la lista de todos los repositorios locales traqueados que no fueron guardados en la master
+-git status: aparece la lista de todos los repositorios locales traqueados que no fueron guardados en la master///seguimiento del estado de los archivos
 -para agregar archivo git add nombre de archivo
 -para agregar mas de una archivo git add . asi con espacio y punto para agregaro todos los archivos a la master
 -git commit: rotula lo que vamos haciendo, tiene que ser descriptivo, preciso y corto... git commit -m''
@@ -15,7 +15,12 @@ git config --global user.email 'juliana.v.lezcano@hotmail.com'
 -git log : para ver el usuario
 -repo remoto: en la nube o GitHub
 -creamos un repositorio en la nube e insertamos en vs el comando remote que sale en el repositorio para enlazar el repo local con el repo remoto y asi congeniar en ambos los futuros cambios(solo lo comitiado) : git remote add origin https://github.com/JulianaLezcano/prueba-node.git
-
+-SIEMPRE PONER GIT ADD . Y GIT COMMIT -M'MSJ' ANTE CADA MODIFICACION
+-PARA SUBIR DICHA MODIF DEL REPO LOCAL AL REPO REMOTO git push origin master
+-----EN GITBASH DIRECTO:
+CREAR CARPETA Mkdir nombrecarpeta
+CREAR ARCHIVO touche nombrearchivo
 */
 
 let modifico;
+let otraModificacion;
