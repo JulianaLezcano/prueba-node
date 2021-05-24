92235f7f8d9d3d4e90ce439255d39322e160dae3
Julianalezcano <juliana.v.lezcano@hotmail.com>
2021-05-24 06:06:11 -0300

    Declaro variable en app.js

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-/*para iniciar proyecto git init
-para agregar archivo git add nombre de archivo
-para agregar mas de una archivo git add .
-git commit: rotula lo que vamos haciendo, tiene que ser descrptivo, preciso y corto...
-primero se pasan credenciales: 
+/*para saber que version tenes es git --version
+repo local: en la compu
+todo en consola:
+para iniciar proyecto  git init .... 
+master: rama principal del proyecto, carpeta inicializadora
+el archivo app.js aparece con una U al lado que significa que no esta guardado/agregado al master
+-git status: aparece la lista de todos los repositorios locales traqueados que no fueron guardados en la master
+-para agregar archivo git add nombre de archivo
+-para agregar mas de una archivo git add . asi con espacio y punto para agregaro todos los archivos a la master
+-git commit: rotula lo que vamos haciendo, tiene que ser descriptivo, preciso y corto... git commit -m''
+-primero se pasan credenciales: 
 git config --global user.name 'Julianalezcano'
 git config --global user.email 'juliana.v.lezcano@hotmail.com'
-git commit -m y el msj, si es mas de una palabra va con 'Agrega archivos base'
+-git commit -m y 'el msj', si es mas de una palabra va con 'Agrega archivos base'
+-git log : para ver el usuario
+-repo remoto: en la nube o GitHub
+-creamos un repositorio en la nube e insertamos en vs el comando remote que sale en el repositorio para enlazar el repo local con el repo remoto y asi congeniar en ambos los futuros cambios(solo lo comitiado) : git remote add origin https://github.com/JulianaLezcano/prueba-node.git
+
+*/
+
+let modifico;
